b6a2cfac6abc96a48d20d8ce151f16409be24f6a
dhopper52 <dhopper.tayyab@gmail.com>
2024-08-05 22:34:41 +0500

    Update index.js
    testing

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 5000;
 const cors = require("cors");
-// const authRouter = require("./routes/authRoute/authRoute");
 const URI = process.env.MONGODB_URI_STRING;
+const authRouter = require("./routes/authRoute/authRoute");
 // const userRoute = require("./routes/userRoute/userRoute");
 // const breakRoute = require("./routes/breakRoute/breakRoute");
-const ipFilter = require("./middleware/ipAuthenticate/ipAuthenticate");
+// const ipFilter = require("./middleware/ipAuthenticate/ipAuthenticate");
 
-app.use(ipFilter);
+// app.use(ipFilter);
 app.use(cors());
 app.use(bodyParser.json());
 
-// app.use("/auth", authRouter);
+app.use("/auth", authRouter);
 // app.use("/user", userRoute);
 // app.use("/break", breakRoute);
 
@@ -35,11 +35,11 @@ const connectDB = async (URI) => {
   }
 };
 
-const start = async () => {
+const startServer = async () => {
   try {
     await connectDB(URI);
     app.listen(PORT, () => {
-      console.log(`app is runing on ${port}`);
+      console.log(`app is runinng on ${port}`);
   });
   } catch (error) {
     console.log(error);
@@ -47,7 +47,7 @@ const start = async () => {
   }
 };
 
-start();
+startServer();
 
 module.exports = app
   
