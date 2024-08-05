09bc67f59c996bdca3fb0ed63e8a722e489866b6
dhopper52 <dhopper.tayyab@gmail.com>
2024-08-05 23:43:06 +0500

    Update index.js
    index login test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 5000;
 const cors = require("cors");
 const URI = process.env.MONGODB_URI_STRING;
-const authRouter = require("./routes/authRoute/authRoute");
+// const authRouter = require("./routes/authRoute/authRoute");
 // const userRoute = require("./routes/userRoute/userRoute");
 // const breakRoute = require("./routes/breakRoute/breakRoute");
 // const ipFilter = require("./middleware/ipAuthenticate/ipAuthenticate");
@@ -15,13 +15,53 @@ const authRouter = require("./routes/authRoute/authRoute");
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/auth", authRouter);
+// app.use("/auth", authRouter);
 // app.use("/user", userRoute);
 // app.use("/break", breakRoute);
 
 app.get('/home', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
 })
+
+app.post("/login", async (req, res) => {
+  const { floorId, password } = req.body;
+  try {
+    const user = await Auth.findOne({ _id: floorId });
+    console.log(user);
+    if (!user) {
+      return res.json({ status: "failed", message: "Error Floor not Found" });
+    }
+    const passwordCompare = await bcrypt.compare(password, user?.password);
+
+    if (!passwordCompare) {
+      return res.json({
+        status: "failed",
+        message: "Please login with correct credentials",
+      });
+    }
+    const data = { _id: user?._id, floorName: user?.floorName };
+    const authToken = jwt.sign(data, jwtString);
+    return res.json({
+      status: "success",
+      data: {
+        _id: user?._id,
+        floorName: user?.floorName,
+        authToken: authToken,
+        role: user?.role,
+      },
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ status: "failed", message: "internal server error" });
+  }
+});
+
+
+
+
+
+
 const connectDB = async (URI) => {
   try {
     await mongoose.connect(URI, {
