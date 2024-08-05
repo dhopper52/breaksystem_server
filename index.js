545fe6aef3a5819b3c54121dddc40d05eb5eaea5
dhopper52 <dhopper.tayyab@gmail.com>
2024-08-05 22:30:18 +0500

    Update index.js
    testing index page

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 5000;
 const cors = require("cors");
-const authRouter = require("./routes/authRoute/authRoute");
+// const authRouter = require("./routes/authRoute/authRoute");
 const URI = process.env.MONGODB_URI_STRING;
-const userRoute = require("./routes/userRoute/userRoute");
-const breakRoute = require("./routes/breakRoute/breakRoute");
+// const userRoute = require("./routes/userRoute/userRoute");
+// const breakRoute = require("./routes/breakRoute/breakRoute");
 const ipFilter = require("./middleware/ipAuthenticate/ipAuthenticate");
 
 app.use(ipFilter);
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/auth", authRouter);
-app.use("/user", userRoute);
-app.use("/break", breakRoute);
+// app.use("/auth", authRouter);
+// app.use("/user", userRoute);
+// app.use("/break", breakRoute);
 
 app.get('/home', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
